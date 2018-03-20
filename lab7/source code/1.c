1f07c4a0c4733d60263bb381dd6855b37004407a
Saroj Shrestha <saroj.shr911@gmail.com>
2018-03-20 16:54:46 +0545

    lab7

---
+++ lab7/source code/1.c
@@ -0,0 +1,21 @@
+/*
+    Author: Saroj Dhwaz Shrestha
+    Roll Number: 171239
+    Date: 2018-19-03 14:57:49
+    File Name: 1.c
+    Objective: 
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct joiningDate{
+    int date, month, year;
+};
+
+struct info{
+    int id;
+    char name[100];
+    struct joiningDate d;
+    int 
+};
